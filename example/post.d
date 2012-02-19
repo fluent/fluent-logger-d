bab623b923762e5c9d726f71a5428228d85a4132
Masahiro Nakagawa <repeatedly@gmail.com>
2012-02-20 06:04:00 +0900

    Add example and candydoc

---
+++ example/post.d
@@ -0,0 +1,27 @@
+import core.thread;
+import std.stdio;
+
+import fluent.logger;
+
+struct S
+{
+    string text = "D";
+    ulong count = 0;
+}
+
+void main()
+{
+    Logger logger = new FluentLogger("debug", FluentLogger.Configuration());
+
+    for (size_t i; i < 100; i++) {
+        if (logger.post("test", S("Testing...", i)))
+            writeln("Pong!");
+        else
+            writeln("Failed");
+
+        Thread.sleep(dur!("msecs")(10));
+    }
+
+    clear(logger);
+    writeln("End");
+}
