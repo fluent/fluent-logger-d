7c55c791f518afbaf5deeaa01de75bc762d9d10d
Masahiro Nakagawa <repeatedly@gmail.com>
2012-12-09 19:07:13 +0900

    Add multi threading example

---
+++ example/post_mt.d
@@ -0,0 +1,37 @@
+import core.thread;
+import std.stdio;
+
+import fluent.logger;
+
+struct S
+{
+    string text = "D";
+    ulong count = 0;
+}
+
+void main()
+{
+    Logger logger = new FluentLogger("debug", FluentLogger.Configuration());
+
+    void worker()
+    {
+        for (size_t i; i < 100; i++) {
+            if (logger.post("test", S("Testing...", i))) {
+                if (i % 10 == 0)
+                    writeln("Pong!");
+            } else {
+                writeln("Failed");
+            }
+
+            Thread.sleep(dur!("msecs")(10));
+        }
+    }
+
+    auto group = new ThreadGroup();
+    foreach (_; 0..5)
+        group.create(&worker);
+    group.joinAll();
+
+    clear(logger);
+    writeln("End");
+}
